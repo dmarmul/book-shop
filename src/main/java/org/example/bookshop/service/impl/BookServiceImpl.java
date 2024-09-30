44ad97522841d61b224f17ce60d578b123863d1a
dmitry <dmarmul228@gmail.com>
2024-09-30 16:22:45 +0300

    Added BookDto and CreateBookRequestDto, EntityNotFoundException classes, mappers, BookController. Added DTOs on the service layer. Removed CommandLineRunner

--- src/main/java/org/example/bookshop/service/impl/BookServiceImpl.java
+++ src/main/java/org/example/bookshop/service/impl/BookServiceImpl.java
@@ -2,23 +2,38 @@ package org.example.bookshop.service.impl;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.example.bookshop.dto.BookDto;
+import org.example.bookshop.dto.CreateBookRequestDto;
+import org.example.bookshop.exception.EntityNotFoundException;
+import org.example.bookshop.mapper.BookMapper;
 import org.example.bookshop.model.Book;
 import org.example.bookshop.repository.BookRepository;
 import org.example.bookshop.service.BookService;
 import org.springframework.stereotype.Service;
 
-@Service
 @RequiredArgsConstructor
+@Service
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public BookDto save(CreateBookRequestDto requestDto) {
+        Book book = bookMapper.toModel(requestDto);
+        return bookMapper.toDto(bookRepository.save(book));
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public BookDto findById(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can't find book by id: " + id));
+        return bookMapper.toDto(book);
+    }
+
+    @Override
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(bookMapper::toDto)
+                .toList();
     }
 }
