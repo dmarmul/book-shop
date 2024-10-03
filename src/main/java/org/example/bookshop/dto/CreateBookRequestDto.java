2f499071848f7658c278e9d5cf1746c3e07edee3
dmitry <dmarmul228@gmail.com>
2024-10-03 17:37:48 +0300

    Replaced NotNull by NotBlank annotation for String fields, remove ToString annotation in CreateBookRequestDto. Used spring instead of hibernate validation. And change return status in CustomGlobalExceptionHandler method to permanent 400

--- src/main/java/org/example/bookshop/dto/CreateBookRequestDto.java
+++ src/main/java/org/example/bookshop/dto/CreateBookRequestDto.java
@@ -1,21 +1,20 @@
 package org.example.bookshop.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
-    @NotNull
+    @NotBlank
     private String isbn;
     @NotNull
     @Min(0)
