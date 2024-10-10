37103ece1b0ed5967564b9aa582e0993eebb3e99
dmitry <dmarmul228@gmail.com>
2024-10-10 19:53:55 +0300

    Sorry, wrote in the comments for every comment I didn't fix, I think the part will be solved when the login method is added

--- src/main/java/org/example/bookshop/security/CustomUserDetailsService.java
+++ src/main/java/org/example/bookshop/security/CustomUserDetailsService.java
@@ -15,6 +15,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) {
         return userRepository.findByEmail(email)
-                .orElseThrow(() -> new EntityNotFoundException("Can't find user " + email));
+                .orElseThrow(() ->
+                        new EntityNotFoundException("Can't find user by email: " + email));
     }
 }
