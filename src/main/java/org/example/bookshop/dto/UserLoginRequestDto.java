752467b385e1dbeca8b230aa39f793edc4ba8ee5
dmitry <dmarmul228@gmail.com>
2024-10-12 15:51:36 +0300

    Added login method. Added jjwt dependencies. Created AuthenticationService, JwtAuthenticationFilter, JwtUtil classes. Added new filter in the SecurityFilterChain bean. Added UserLoginRequestDto and UserLoginResponseDto.

---
+++ src/main/java/org/example/bookshop/dto/UserLoginRequestDto.java
@@ -0,0 +1,16 @@
+package org.example.bookshop.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserLoginRequestDto(
+        @NotBlank
+        @Size(min = 6, max = 30)
+        @Email
+        String email,
+        @NotBlank
+        @Size(min = 6, max = 30)
+        String password
+) {
+}
